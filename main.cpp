8ec51ba9d33c3810a75aa2b0c74f137d84c26875
CristianPreda1609 <cristianpreda222@gmail.com>
2023-11-13 15:04:29 +0200

    Al patrulea commit

--- main.cpp
+++ main.cpp
@@ -63,6 +63,22 @@ int main() {
     #ifdef __linux__
     XInitThreads();
     #endif
+    // Create the main window
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
     startGame();
 
 
